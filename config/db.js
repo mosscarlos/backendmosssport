335ac4b1a63bf1e6a2cd4346def8bff4afb208d3
juanchoque <juan.choque.m@tecsup.edu.pe>
2025-02-15 02:45:29 -0500

    primer commit

--- config/db.js
+++ config/db.js
@@ -1,12 +1,24 @@
-const mysql = require('mysql2');
+require('dotenv').config();
+const mysql = require('mysql2/promise');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  database: 'deportivo',
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-const dbPromise = db.promise();
+pool.getConnection()
+  .then(connection => {
+    console.log('✅ Conectado a MySQL en Railway');
+    connection.release();
+  })
+  .catch(err => {
+    console.error('❌ Error conectando a la BD:', err);
+  });
 
-module.exports = dbPromise;
+module.exports = pool;
